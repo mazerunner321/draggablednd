4023ef270e4b23ba00b8a8e75901ae84fa817e9b
mazerunner321 <prajwalhg321@gmail.com>
2023-09-13 16:44:17 +0530

    final

--- src/App.js
+++ src/App.js
@@ -5,31 +5,31 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 const DATA = [
   {
     id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
-    name: "Walmart",
+    name: "Reliance Fresh",
     items: [
-      { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "3% Milk" },
+      { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "Milk" },
       { id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525", name: "Butter" },
     ],
     tint: 1,
   },
   {
     id: "487f68b4-1746-438c-920e-d67b7df46247",
-    name: "Indigo",
+    name: "More",
     items: [
       {
         id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
-        name: "Designing Data Intensive Applications",
+        name: "Chocolates",
       },
-      { id: "5bee94eb-6bde-4411-b438-1c37fa6af364", name: "Atomic Habits" },
+      { id: "5bee94eb-6bde-4411-b438-1c37fa6af364", name: "Wheat" },
     ],
     tint: 2,
   },
   {
     id: "25daffdc-aae0-4d73-bd31-43f73101e7c0",
-    name: "Lowes",
+    name: "IKEA",
     items: [
-      { id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc", name: "Workbench" },
-      { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Hammer" },
+      { id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc", name: "Chairs" },
+      { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Desk" },
     ],
     tint: 3,
   },
@@ -38,14 +38,62 @@ const DATA = [
 function App() {
   const [stores, setStores] = useState(DATA || []);
 
+  const handleDragDrop = (results) => {
+    const { source, destination, type } = results;
+
+    if (!destination) return;
+
+    if (
+      source.draggableId === destination.draggableId &&
+      source.index === destination.index
+    )
+      return;
+
+    if (type == "group") {
+      const reorderedStores = [...stores];
+
+      const sourceIndex = source.index;
+      const destinationIndex = destination.index;
+
+      const [removedStore] = reorderedStores.splice(sourceIndex, 1);
+      reorderedStores.splice(destinationIndex, 0, removedStore);
+
+      return setStores(reorderedStores);
+    }
+
+    const storeSourceIndex = stores.findIndex(
+      (store) => store.id === source.droppableId
+    );
+    const storeDestinationIndex = stores.findIndex(
+      (store) => store.id === destination.droppableId
+    );
+    const newSourceItems = [...stores[storeSourceIndex].items];
+    const newDestinationItems =
+      source.droppableId !== destination.droppableId
+        ? [...stores[storeDestinationIndex].items]
+        : newSourceItems;
+
+    const [deletedItem] = newSourceItems.splice(source.index, 1);
+    newDestinationItems.splice(destination.index, 0, deletedItem);
+
+    const newStores = [...stores];
+    newStores[storeSourceIndex] = {
+      ...stores[storeSourceIndex],
+      items: newSourceItems,
+    };
+
+    newStores[storeDestinationIndex] = {
+      ...stores[storeDestinationIndex],
+      items: newDestinationItems,
+    };
+
+    setStores(newStores);
+  };
+
   return (
     <div className="App layout__wrapper">
       <div className="card">
-        <DragDropContext
-          onDragEnd={() => {
-            console.log("Drag Event Triggered");
-          }}
-        >
+        <DragDropContext onDragEnd={handleDragDrop}>
           <div className="header">
             <h1>What to Shop?</h1>
           </div>
@@ -60,22 +108,53 @@ function App() {
                   >
                     {(provided) => (
                       <div
-                        className="store-container"
                         {...provided.dragHandleProps}
                         {...provided.draggableProps}
                         ref={provided.innerRef}
                       >
-                        <h3>{store.name}</h3>
+                        <StoreList {...store} />
                       </div>
                     )}
                   </Draggable>
                 ))}
+                {provided.placeholder}
               </div>
             )}
           </Droppable>
         </DragDropContext>
       </div>
     </div>
+  );
+}
+
+function StoreList({ name, items, id }) {
+  return (
+    <Droppable droppableId={id}>
+      {(provided) => (
+        <div {...provided.droppableProps} ref={provided.innerRef}>
+          <div className="store-container">
+            <h3>{name}</h3>
+          </div>
+          <div className="items-container">
+            {items?.map((item, i) => (
+              <Draggable draggableId={item.id} index={i} key={item.id}>
+                {(provided) => (
+                  <div
+                    className="item-container"
+                    {...provided.dragHandleProps}
+                    {...provided.draggableProps}
+                    ref={provided.innerRef}
+                  >
+                    <h4>{item.name}</h4>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        </div>
+      )}
+    </Droppable>
   );
 }
 
